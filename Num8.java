9d3393c22f6d0d48f5190f5e9de3e45c9d3c7431
Dhinesh M <dhineshmuthuraja84@gmail.com>
2026-06-28 20:16:00 +0530

    Create Num8.java

---
+++ Num8.java
@@ -0,0 +1,5 @@
+"C:\Program Files\Java\jdk-25.0.2\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2025.2.6.1\lib\idea_rt.jar=64427" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\dhine\IdeaProjects\Code\out\production\Code Code
+234
+9
+
+Process finished with exit code 0
